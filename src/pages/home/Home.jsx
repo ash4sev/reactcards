4595f9e864ce1044199fc4996c8de6fa5224b6ca
ash4sev <ashysev4@gmail.com>
2021-10-28 19:03:34 -0300

    configurando rotas

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Home() {
+    return (
+        <div>
+
+            <h1> pagina home </h1>
+            
+        </div>
+    )
+}
+
+export default Home
